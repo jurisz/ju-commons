9297df683c4130b7f5d80dc31bcdb7ddb2b9ea1a
juriszubkans <juris.zubkans@gmail.com>
2016-07-06 15:26:18 +0300

    upgraded camel version, next release

--- juz-commons-base/src/test/java/integration/IntegrationTestingConfiguration.java
+++ juz-commons-base/src/test/java/integration/IntegrationTestingConfiguration.java
@@ -1,6 +1,10 @@
 package integration;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
 import org.juz.common.config.ScanPersistencePackages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +16,11 @@ public class IntegrationTestingConfiguration {
 	@Bean
 	ScanPersistencePackages scanPersistencePackages() {
 		return new ModelPersistenceScan();
+	}
+
+	@Bean
+	@Autowired
+	CamelContext camelContext(ApplicationContext applicationContext) {
+		return new SpringCamelContext(applicationContext);
 	}
 }
